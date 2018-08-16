5799ec721f3e10655bd4080d21ecea505fdb4a11
MatanHu <MatanHuja@gmail.com>
2018-08-17 01:50:12 +0300

    Convert to MVC design pattern

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by MatanHuja on 17/08/2018.
+//  Copyright © 2018 Matan Huja. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
